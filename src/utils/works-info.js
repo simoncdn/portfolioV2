684df77c5620dc011cd3574b8ecc3144d20546d2
sdn <cdna.simon@gmail.com>
2024-02-29 15:47:31 +0100

    ✏️ fix(utils): Change works date info

--- src/utils/works-info.js
+++ src/utils/works-info.js
@@ -7,7 +7,7 @@ export const worksInfo = [
       'Klay merges the craft of pottery with a modern touch, offering unique creations and interactive workshops to immerse enthusiasts in the art of ceramics. Their goal is to make this art accessible to everyone through an online sales platform and a workshop sign-up system, akin to a newsletter, enriching the cultural experience of their customers.',
     subDescription:
       'My mission was to develop an e-commerce site for Klay, designed to showcase its products and facilitate workshop sign-ups through email registration. This project aimed to blend appealing aesthetics with intuitive navigation, thereby creating a user interface that highlights the art of pottery and encourages user engagement through a seamless and enriching online experience.',
-    date: '2024',
+    date: 'INCOMING 2024',
     url: 'https://klay-craft.netlify.app',
     laguages: ['Nuxt.js', 'TypeScript'],
     tools: ['Tailwind', 'Pinia', 'Figma']
@@ -20,7 +20,7 @@ export const worksInfo = [
       'Forma stands out as an innovative and user-friendly SaaS solution, targeting legal and accounting professionals. Its mission aims to simplify exchanges and ensure efficient tracking of legal formalities, enriched by a range of additional features. Among these advantages, it offers the ability to interact with clients via a dedicated messaging system, to share, preview, and securely approve documents, and to schedule activities accompanied by useful reminders, thereby improving time management and organizational efficiency.',
     subDescription:
       "In my role, I've been entrusted with leading the development of prototypes and contributing to the full-stack development of the platform, working closely with the backend developer. This opportunity has significantly enriched my professional experience, immersing me deeply in the process of creation and technological development, and presenting various challenges that have stimulated my ability to innovate and solve problems.",
-    date: '2023',
+    date: 'MAY 2023 - TODAY',
     url: 'https://forma.legal/',
     laguages: ['Next.js', 'TypeScript'],
     tools: ['Tailwind', 'Zustand', 'React-query', 'Zod', 'React-hook-form', 'Figma']
@@ -33,7 +33,7 @@ export const worksInfo = [
       'In a world where convenience and speed reign supreme, the idea of allowing customers to place their orders online with disconcerting ease seemed not only logical but essential. This dashboard, conceived as an elegant and functional interface, invites users to a smooth and enjoyable online ordering experience, turning each menu selection into a captivating culinary adventure, accessible at their fingertips.',
     subDescription:
       'This innovative user dashboard project for Crazee Burger was designed as part of a practical training program, aiming to combine theoretical knowledge with real-world application. It allowed me to develop essential skills in designing interactive systems.',
-    date: '2023',
+    date: 'JAN 2023 - MAY 2023',
     url: 'https://crazee-burger-ki3o6okfp-simoncdn.vercel.app/',
     laguages: ['React.js'],
     tools: ['Styled-components', 'Firebase', 'Jest', 'Context', 'Figma']
@@ -46,7 +46,7 @@ export const worksInfo = [
       'As the Community Manager, Workshopbya aimed to create a showcase website that would allow clients to get acquainted with its dynamic and unique identity, while also offering them the chance to explore a wide range of services. The goal was to develop an interactive space, enhanced by an exciting blog that would highlight its expertise and enthusiasm, thus providing a glimpse into its creative and stimulating universe.',
     subDescription:
       'As the person responsible for creating the prototype and developing the web for Workshopbya, I had to encapsulate its unique essence by designing an intuitive user interface that was aesthetically pleasing, enriched with interactive elements and a responsive design for full accessibility across various devices. This role allowed me to refine my web development and UI/UX design skills, emphasizing the importance of storytelling to effectively engage visitors.',
-    date: '2022',
+    date: 'DEC 2022 - JAN 2023',
     url: 'https://www.workshopbya.com/',
     laguages: ['Next.js', 'TypeScript'],
     tools: ['Tailwind', 'Sanity.io', 'Sendgrid', 'Framer-motion', 'Figma']
